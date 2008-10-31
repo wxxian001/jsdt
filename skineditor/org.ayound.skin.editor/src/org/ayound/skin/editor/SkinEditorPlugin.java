3376e0b51f125d8236f0180bc4cdb8a19ea8fe40
ayound <ayound@90c94b88-a700-11dd-a8c6-677c70d10fe4>
2008-10-31 15:12:50 +0000

    初始导入。

---
+++ skineditor/org.ayound.skin.editor/src/org/ayound/skin/editor/SkinEditorPlugin.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ *
+ *==============================================================================
+ *
+ * Copyright (c) 2008-2011 ayound@gmail.com 
+ * This program and the accompanying materials
+ * are made available under the terms of the Apache License 2.0 
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * All rights reserved.
+ * 
+ * Created on 2008-10-31
+ *******************************************************************************/
+
+package org.ayound.skin.editor;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SkinEditorPlugin extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.ayound.skin.editor";
+
+	// The shared instance
+	private static SkinEditorPlugin plugin;
+
+	/**
+	 * The constructor
+	 */
+	public SkinEditorPlugin() {
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static SkinEditorPlugin getDefault() {
+		return plugin;
+	}
+
+}
