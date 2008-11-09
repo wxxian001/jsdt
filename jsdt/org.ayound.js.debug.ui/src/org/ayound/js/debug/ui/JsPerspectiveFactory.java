512d36f0474bfa34da3c52e0c9e8d9bbab5a2a9e
ayound <ayound@90c94b88-a700-11dd-a8c6-677c70d10fe4>
2008-11-09 13:57:01 +0000

    update: add expression view

--- jsdt/org.ayound.js.debug.ui/src/org/ayound/js/debug/ui/JsPerspectiveFactory.java
+++ jsdt/org.ayound.js.debug.ui/src/org/ayound/js/debug/ui/JsPerspectiveFactory.java
@@ -25,18 +25,24 @@ public class JsPerspectiveFactory implements IPerspectiveFactory {
 	 * @see IPerspectiveFactory#createInitialLayout(IPageLayout)
 	 */
 	public void createInitialLayout(IPageLayout layout) {
-		IFolderLayout consoleFolder = layout.createFolder("bottom", IPageLayout.BOTTOM, (float)0.75, layout.getEditorArea());
+		IFolderLayout consoleFolder = layout.createFolder("bottom",
+				IPageLayout.BOTTOM, (float) 0.75, layout.getEditorArea());
 		consoleFolder.addView(IPageLayout.ID_TASK_LIST);
-		
-		IFolderLayout navFolder= layout.createFolder("top", IPageLayout.TOP, (float) 0.45, layout.getEditorArea());
+
+		IFolderLayout navFolder = layout.createFolder("top", IPageLayout.TOP,
+				(float) 0.45, layout.getEditorArea());
 		navFolder.addView(IDebugUIConstants.ID_DEBUG_VIEW);
 		navFolder.addPlaceholder(IPageLayout.ID_RES_NAV);
-		
-		IFolderLayout toolsFolder= layout.createFolder("right", IPageLayout.RIGHT, (float) 0.50, "top");
+
+		IFolderLayout toolsFolder = layout.createFolder("right",
+				IPageLayout.RIGHT, (float) 0.40, "top");
 		toolsFolder.addView(IDebugUIConstants.ID_VARIABLE_VIEW);
 		toolsFolder.addView(IDebugUIConstants.ID_BREAKPOINT_VIEW);
-		toolsFolder.addView("org.ayound.js.debug.ui.ExpressionView");
-		IFolderLayout resourceFolder= layout.createFolder("resource", IPageLayout.RIGHT, (float) 0.75, layout.getEditorArea());
+		IFolderLayout expressionFolder = layout.createFolder("expression",
+				IPageLayout.RIGHT, (float) 0.50, "right");
+		expressionFolder.addView("org.ayound.js.debug.ui.ExpressionView");
+		IFolderLayout resourceFolder = layout.createFolder("resource",
+				IPageLayout.RIGHT, (float) 0.75, layout.getEditorArea());
 		resourceFolder.addView("org.ayound.js.debug.ui.resourceView");
 	}
 }
