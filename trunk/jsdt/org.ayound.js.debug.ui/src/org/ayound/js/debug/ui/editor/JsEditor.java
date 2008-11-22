b0bbcfd8aca418cfdc8ba91d9340bd94b79b9c6d
ayound <ayound@90c94b88-a700-11dd-a8c6-677c70d10fe4>
2008-11-22 14:18:14 +0000

    update:add error report

--- trunk/jsdt/org.ayound.js.debug.ui/src/org/ayound/js/debug/ui/editor/JsEditor.java
+++ trunk/jsdt/org.ayound.js.debug.ui/src/org/ayound/js/debug/ui/editor/JsEditor.java
@@ -13,9 +13,22 @@
  *******************************************************************************/
 package org.ayound.js.debug.ui.editor;
 
+import org.ayound.js.debug.engine.EngineManager;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditor;
+import org.mozilla.javascript.EvaluatorException;
 
 public class JsEditor extends AbstractDecoratedTextEditor {
+
+	private static final String ERROR_MARKER_ID = "org.ayound.js.debug.errorMarker";
+
 	private ColorManager colorManager;
 
 	/**
@@ -33,5 +46,68 @@ public class JsEditor extends AbstractDecoratedTextEditor {
 	public void dispose() {
 		colorManager.dispose();
 		super.dispose();
+
 	}
+
+	@Override
+	protected void doSetInput(IEditorInput input) throws CoreException {
+		super.doSetInput(input);
+		validateAndMark();
+	}
+
+	@Override
+	public void doSave(IProgressMonitor progressMonitor) {
+		super.doSave(progressMonitor);
+		validateAndMark();
+	}
+
+	protected void validateAndMark() {
+		IFile file = ((IFileEditorInput) getEditorInput()).getFile();
+		try {
+			IMarker[] markers = file.findMarkers(ERROR_MARKER_ID, true,
+					IResource.DEPTH_INFINITE);
+			for (IMarker marker : markers) {
+				marker.delete();
+			}
+		} catch (CoreException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		IDocument document = getDocumentProvider()
+				.getDocument(getEditorInput());
+		String text = document.get();
+		if (text.trim().startsWith("<")) {
+			try {
+				EngineManager.getEngine().compileHtml("_temp.js", text);
+			} catch (EvaluatorException e) {
+				handleError(e, true);
+			}
+		} else {
+			try {
+				EngineManager.getEngine().compileJs("_temp.js", text);
+			} catch (EvaluatorException e) {
+				handleError(e, true);
+			}
+		}
+
+	}
+
+	protected void handleError(EvaluatorException e, boolean isFatal) {
+
+		IFile file = ((IFileEditorInput) getEditorInput()).getFile();
+		;
+		try {
+			IMarker marker = file.createMarker(ERROR_MARKER_ID);
+			marker.setAttribute(IMarker.SEVERITY, IMarker.SEVERITY_ERROR);
+			marker
+					.setAttribute(IMarker.LOCATION, file.getFullPath()
+							.toString());
+			marker.setAttribute(IMarker.MESSAGE, "[Error]: " + e.details());
+			marker.setAttribute(IMarker.LINE_NUMBER, e.lineNumber());
+		} catch (CoreException e1) {
+			e1.printStackTrace();
+		}
+
+	}
+
 }
