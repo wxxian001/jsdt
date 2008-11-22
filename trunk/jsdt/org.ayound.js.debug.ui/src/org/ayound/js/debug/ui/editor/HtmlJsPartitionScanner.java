5f81a8d9c75541691552f9b77070bc1f6c737cd5
ayound <ayound@90c94b88-a700-11dd-a8c6-677c70d10fe4>
2008-11-22 14:46:33 +0000

    update:update color

--- trunk/jsdt/org.ayound.js.debug.ui/src/org/ayound/js/debug/ui/editor/HtmlJsPartitionScanner.java
+++ trunk/jsdt/org.ayound.js.debug.ui/src/org/ayound/js/debug/ui/editor/HtmlJsPartitionScanner.java
@@ -1,6 +1,12 @@
 package org.ayound.js.debug.ui.editor;
 
-import org.eclipse.jface.text.rules.*;
+import org.eclipse.jface.text.rules.IPredicateRule;
+import org.eclipse.jface.text.rules.IToken;
+import org.eclipse.jface.text.rules.MultiLineRule;
+import org.eclipse.jface.text.rules.RuleBasedPartitionScanner;
+import org.eclipse.jface.text.rules.SingleLineRule;
+import org.eclipse.jface.text.rules.Token;
+
 
 public class HtmlJsPartitionScanner extends RuleBasedPartitionScanner {
 	public final static String HTMLJS_COMMENT = "__html_js_comment";
@@ -11,11 +17,12 @@ public class HtmlJsPartitionScanner extends RuleBasedPartitionScanner {
 		IToken htmlJsComment = new Token(HTMLJS_COMMENT);
 		IToken tag = new Token(HTML_TAG);
 
-		IPredicateRule[] rules = new IPredicateRule[3];
+		IPredicateRule[] rules = new IPredicateRule[4];
 
 		rules[0] = new MultiLineRule("<!--", "-->", htmlJsComment);
-		rules[1] = new MultiLineRule("/*","*/",htmlJsComment);
-		rules[2] = new SingleLineRule("//","",htmlJsComment);
+		rules[1] = new TagRule(tag);
+		rules[2] = new MultiLineRule("/*","*/",htmlJsComment);
+		rules[3] = new SingleLineRule("//","",htmlJsComment);
 		setPredicateRules(rules);
 	}
 }
