a1175af2c3f43aeeb37b2d7230fb2abd5a0ef0ff
ayound <ayound@90c94b88-a700-11dd-a8c6-677c70d10fe4>
2009-04-04 04:17:04 +0000

    初始导入。

---
+++ jsdt-s/jsdt-engine/src/org/ayound/js/debug/engine/EngineManager.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ *
+ *==============================================================================
+ *
+ * Copyright (c) 2008-2011 ayound@gmail.com 
+ * This program and the accompanying materials
+ * are made available under the terms of the Apache License 2.0 
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * All rights reserved.
+ * 
+ * Created on 2008-10-26
+ *******************************************************************************/
+package org.ayound.js.debug.engine;
+
+public class EngineManager {
+
+	private static IJsEngine ENGINE = new JsEngineImpl();
+	/**
+	 * get default javascript engine
+	 * @return
+	 */
+	public static IJsEngine getEngine() {
+		return ENGINE;
+	}
+}
